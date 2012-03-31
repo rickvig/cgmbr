494789f65a39db66a4b7b907e6c4626d91f89430
rickuev <rickuev@ae09564d-8fff-86f1-7166-917943bc23b3>
2012-03-31 06:30:16 +0000

    acertos

--- cgmbr/grails-app/conf/BootStrap.groovy
+++ cgmbr/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,32 @@
+import com.acesso.ShiroRole;
+import com.acesso.ShiroUser;
+import org.apache.shiro.crypto.hash.Sha256Hash
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+	def init = { servletContext ->
+
+		def adminRole = ShiroRole.findByName("Administrador")
+
+		if(!adminRole){
+			adminRole = new ShiroRole(name: 'Administrador')
+			adminRole.addToPermissions("*:*")
+			adminRole.save()
+		}
+
+		def admin = ShiroUser.findByUsername('admin')
+
+		if(!admin){
+			admin = new ShiroUser(username: 'admin', passwordHash: new Sha256Hash('admin').toHex())
+			admin.save()
+			adminRole.addToUsers(admin)
+			adminRole.save()
+		}
+
+//		def user = new ShiroUser(username: "user123", passwordHash: new Sha256Hash("password").toHex())
+//		user.addToPermissions("*:*")
+//		user.save()
+	}
+	def destroy = {
+	}
 }
